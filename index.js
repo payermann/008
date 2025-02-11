7cbadbb1d946241866a21ac8cb5e7e0a6beddc74
Felix Fischer <payermann@gmail.com>
2025-02-11 12:58:03 +0100

    zweite Aufgabe

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ for (wort of woerter) {
   gesamtlaenge += wort.length;
 }
 console.log(`Gesamtlänge aller Wörter: ${gesamtlaenge}`);
+
+let zahlen = [4, 8, 15, 16, 23, 42];
+
+let durchschnitt = 0;
+let summe = 0;
+
+for (zahl of zahlen) {
+  summe += zahl;
+}
+console.log(`Summe: ${summe}`);
+console.log(`Durschnitt: ${Math.round((summe / zahlen.length) * 100) / 100}`);
